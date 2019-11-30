52e3524b2fb0b0bb91129d08803acce5c9a57ede
Nandhito Sam Pramudana <sampramudana12@gmail.com>
2019-12-01 00:25:19 +0700

    adding missing file

--- app/src/main/java/com/example/ujikom/activity/home/HomeActivity.java
+++ app/src/main/java/com/example/ujikom/activity/home/HomeActivity.java
@@ -5,6 +5,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -33,7 +34,7 @@ public class HomeActivity extends Fragment {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.item1:
-//                startActivity(new Intent(this, TambahSiswaActivity.class));
+                startActivity(new Intent(getActivity(), TambahSiswaActivity.class));
                 return true;
         }
         return super.onOptionsItemSelected(item);
--- app/src/main/java/com/example/ujikom/activity/home/TambahSiswaActivity.java
+++ app/src/main/java/com/example/ujikom/activity/home/TambahSiswaActivity.java
@@ -1,16 +1,48 @@
 package com.example.ujikom.activity.home;
 
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.example.ujikom.R;
+import com.example.ujikom.network.ApiClient;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class TambahSiswaActivity extends AppCompatActivity {
+
+    @BindView(R.id.edtNamaSiswa)
+    EditText edtNamaSiswa;
+    @BindView(R.id.btnTambahSiswa)
+    Button btnTambahSiswa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambah_siswa);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnTambahSiswa)
+    public void onViewClicked() {
+
+        String namaSiswa = edtNamaSiswa.getText().toString();
+
+        if (TextUtils.isEmpty(namaSiswa)) {
+            Toast.makeText(this, "The fill cannot be empty", Toast.LENGTH_SHORT).show();
+        } else {
+            tambahSiswa(namaSiswa);
+        }
+
+    }
+
+    private void tambahSiswa(String paramNamaSiswa) {
+//        ApiClient
     }
 }
